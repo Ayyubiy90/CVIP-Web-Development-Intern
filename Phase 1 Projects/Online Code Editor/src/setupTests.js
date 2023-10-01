d58807bf58d1f73526364fb43d5d8b71ad010117
ayb <ayyubiy67@gmail.com>
2023-10-01 10:08:45 +0100

    chore: Add jest-dom for enhanced DOM assertions in tests - Include @testing-library/jest-dom library in testing setup. - Enhance test assertions with custom matchers for DOM nodes.

---
+++ Phase 1 Projects/Online Code Editor/src/setupTests.js
@@ -0,0 +1,6 @@
+// jest-dom custom jest matchers fooor asserting on DOM nodes.
+// allows you to do things like
+// except(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+
+import "@testing-library/jest-dom";
